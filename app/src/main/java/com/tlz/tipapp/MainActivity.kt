be30e50b2c7aadbd38a1d9debb99b1c94592d3e9
TheLordZedd <pcgpod@yahoo.com>
2023-02-22 10:11:15 -0600

    Initial commit

---
+++ app/src/main/java/com/tlz/tipapp/MainActivity.kt
@@ -0,0 +1,101 @@
+package com.tlz.tipapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.tlz.tipapp.ui.theme.TipAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApp {
+                TopHeader()
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(content : @Composable () -> Unit) {
+    TipAppTheme {
+        // A surface container using the 'background' color from the theme
+       Surface(color = MaterialTheme.colors.background) {
+           content()
+       }
+    }
+}
+
+@Preview
+@Composable
+fun TopHeader(totalPerPerson : Double = 134.0) {
+    Surface(modifier = Modifier
+        .fillMaxWidth()
+        .height(150.dp)
+        .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp))),
+        color = Color(0xFFE9D7F7)
+    ) {
+        Column(modifier = Modifier.padding(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+        ) {
+            val total = "%.2f".format(totalPerPerson)
+
+            Text(text = "Total Per Person",
+                style = MaterialTheme.typography.h5)
+            Text(text = "$$totalPerPerson",
+                style = MaterialTheme.typography.h4,
+                fontWeight = FontWeight.Bold)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MainContent() {
+    Surface(modifier = Modifier
+        .padding(2.dp)
+        .fillMaxWidth(),
+            shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+            border = BorderStroke(width = 1.dp, color = Color.LightGray)
+    ) {
+        Column() {
+            Text(text = "Helloooooo")
+            Text(text = "Helloooooo")
+            Text(text = "Helloooooo")
+        }
+    }
+}
+
+
+
+
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TipAppTheme {
+        MyApp {
+            Text("Hello Again")
+        }
+    }
+}
