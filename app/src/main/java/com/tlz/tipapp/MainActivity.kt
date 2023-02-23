746313edf8e7f5a557d3c5bd363266ff3439ad68
TheLordZedd <pcgpod@yahoo.com>
2023-02-23 10:32:10 -0600

    UI Adjustments, add main content and input field

--- app/src/main/java/com/tlz/tipapp/MainActivity.kt
+++ app/src/main/java/com/tlz/tipapp/MainActivity.kt
@@ -9,17 +9,23 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.tlz.tipapp.components.InputField
 import com.tlz.tipapp.ui.theme.TipAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,7 +33,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                TopHeader()
+//                TopHeader()
+                MainContent()
             }
         }
     }
@@ -67,9 +74,20 @@ fun TopHeader(totalPerPerson : Double = 134.0) {
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Preview
 @Composable
 fun MainContent() {
+    val totalBillState = remember {
+        mutableStateOf("")
+    }
+
+    val validState = remember(totalBillState.value) {
+        totalBillState.value.trim().isNotEmpty()
+    }
+
+    val keyboardController = LocalSoftwareKeyboardController.current
+
     Surface(modifier = Modifier
         .padding(2.dp)
         .fillMaxWidth(),
@@ -77,9 +95,17 @@ fun MainContent() {
             border = BorderStroke(width = 1.dp, color = Color.LightGray)
     ) {
         Column() {
-            Text(text = "Helloooooo")
-            Text(text = "Helloooooo")
-            Text(text = "Helloooooo")
+
+            InputField(valueState = totalBillState,
+                labelId = "Enter Bill",
+                enabled = true,
+                isSingleLine = true,
+                onAction = KeyboardActions {
+                    if (!validState) return@KeyboardActions
+                    
+
+                    keyboardController?.hide()
+                })
         }
     }
 }
